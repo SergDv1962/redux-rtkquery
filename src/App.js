ce3dc74c6b80bded8043e20e4ba398d71279eca5
unknown <dsv19621227@gmail.com>
2024-05-27 13:08:21 +0300

    add select to limit, getGoods

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-
+import { useState } from 'react';
 import { useGetGoodsQuery } from './redux';
 
 function App() {
-  const {data = [], isLoading} = useGetGoodsQuery();
+  const [count, setCount] = useState('');
+  const {data = [], isLoading} = useGetGoodsQuery(count);
 
   if(isLoading) return <h1>Loading...</h1>
 
   return (
     <div>
+      <div>
+        <select value={count} onChange={e =>setCount(e.target.value)}>
+          <option value=''>All</option>
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+        </select>
+      </div>
       <ul>
         {data.map(item => (
           <li key={item.id}>
